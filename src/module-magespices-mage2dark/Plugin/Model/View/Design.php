40322c12c01af6e9bd134f7de0a137c6f3b2c4f0
Rav [RedChamps] <rav@redchamps.com>
2019-12-11 14:19:43 +0530

    Added time slot selection setting + changed as per feedback on pull request

--- src/module-magespices-mage2dark/Plugin/Model/View/Design.php
+++ src/module-magespices-mage2dark/Plugin/Model/View/Design.php
@@ -7,6 +7,8 @@ use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 
 class Design
 {
+    const BASE_CONFIG_PATH = "mage2dark/settings/";
+
     protected $scopeConfig;
 
     protected $timeZone;
@@ -23,16 +25,14 @@ class Design
 
     /**
      * @param \Magento\Theme\Model\View\Design $subject
-     * @param callable $proceed
      * @param string|null $area
-     * @param array $params
-     * @return
+     * @param $result
+     * @return string
      */
-    public function aroundGetConfigurationDesignTheme(
+    public function afterGetConfigurationDesignTheme(
         \Magento\Theme\Model\View\Design $subject,
-        callable $proceed,
-        $area = null,
-        array $params = []
+        $result,
+        $area = null
     ) {
         if (!$area) {
             $area = $subject->getArea();
@@ -40,22 +40,30 @@ class Design
         if ($area == Area::AREA_ADMINHTML && $this->getThemeSelectionConfig() == "dark") {
             return "MageSpices/Mage2Dark";
         }
-        return $proceed($area, $params);
+        return $result;
     }
 
     protected function getThemeSelectionConfig()
     {
         if (!$this->themeSelectionConfig) {
-            $this->themeSelectionConfig = $this->scopeConfig->getValue('mage2dark/settings/theme_selection');
+            $this->themeSelectionConfig = $this->getConfig('theme_selection');
             if ($this->themeSelectionConfig == "auto") {
-                $currentHour = $this->timeZone->date()->format("H");
-                if (($currentHour >= 19 || $currentHour < 7)) {
-                    $this->themeSelectionConfig = "dark";
-                } else {
+                $currentDate = $this->timeZone->date();
+                $currentTime = ($currentDate->format("H")*60)+$currentDate->format("i");
+                $lightThemeStartTime = $this->getConfig("light_theme_time_from");
+                $lightThemeEndTime = $this->getConfig("light_theme_time_to");
+                if ($currentTime >= $lightThemeStartTime && $currentTime <= $lightThemeEndTime) {
                     $this->themeSelectionConfig = "light";
+                } else {
+                    $this->themeSelectionConfig = "dark";
                 }
             }
         }
         return $this->themeSelectionConfig;
+    }
+
+    protected function getConfig($path)
+    {
+        return $this->scopeConfig->getValue(self::BASE_CONFIG_PATH.$path);
     }
 }
